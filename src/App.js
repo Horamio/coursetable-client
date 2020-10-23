a32c00b8cfb56a2fed675a3bd4b4b60dccc36bdf
Ricardo Yrupailla <ry.yrupailla@gmail.com>
2020-10-22 22:59:00 -0500

    Add styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import styled from "@emotion/styled";
+import React from "react";
+import CoursePicker from "./components/CoursePicker";
+
+const StyledDiv = styled.div`
+  display: flex;
+`;
 
 function App() {
   return (
     <div className="App">
-      CourseTable
+      <h1>Horamio</h1>
+
+      <StyledDiv>
+        <CoursePicker />
+        <CoursePicker />
+      </StyledDiv>
     </div>
   );
 }
