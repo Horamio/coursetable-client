78d8e4eaaf5cc928cf767caa19a5ecf4543e4e34
Ricardo Yrupailla <ry.yrupailla@gmail.com>
2020-10-22 23:18:40 -0500

    Add course details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import styled from "@emotion/styled";
 import React from "react";
+import CourseDetails from "./components/CourseDetails";
 import CoursePicker from "./components/CoursePicker";
 
 const StyledDiv = styled.div`
   display: flex;
+  padding: 50px;
 `;
 
 function App() {
@@ -13,7 +15,7 @@ function App() {
 
       <StyledDiv>
         <CoursePicker />
-        <CoursePicker />
+        <CourseDetails />
       </StyledDiv>
     </div>
   );
