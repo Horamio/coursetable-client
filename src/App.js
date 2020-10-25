32ee181cc6328081d0a21152f71177e3201dc47c
Ricardo Yrupailla <ry.yrupailla@gmail.com>
2020-10-25 00:21:31 -0500

    Create course picker component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import styled from "@emotion/styled";
 import React from "react";
-import CourseDetails from "./components/CourseDetails";
 import CoursePicker from "./components/CoursePicker";
-import CourseCalendar from "./components/CourseCalendar";
-import CourseTable from "./components/CourseTable";
-
-const StyledDiv = styled.div`
-  display: flex;
-  padding: 50px;
-`;
 
 function App() {
   return (
     <div className="App">
       <h1>Horamio</h1>
 
-      <StyledDiv>
-        <CoursePicker />
-        <CourseDetails />
-      </StyledDiv>
-      <StyledDiv>
-        <CourseTable />
-        <CourseCalendar />
-      </StyledDiv>
+      <CoursePicker />
     </div>
   );
 }
