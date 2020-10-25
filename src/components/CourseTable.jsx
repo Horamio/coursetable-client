8b91d996e3e4301bec0c7dce7465df3eb86461a7
Ricardo Yrupailla <ry.yrupailla@gmail.com>
2020-10-25 11:48:48 -0500

    Add pagination

--- src/components/CourseTable.jsx
+++ src/components/CourseTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
 //components
@@ -8,11 +8,16 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import TablePagination from "@material-ui/core/TablePagination";
 import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 650,
+    paddingTop: "25px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
   },
 });
 
@@ -25,6 +30,17 @@ const headerCells = [
 
 export default function CourseTable({ isLoading, courses }) {
   const classes = useStyles();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   if (isLoading || !courses) return null;
 
@@ -44,20 +60,31 @@ export default function CourseTable({ isLoading, courses }) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {courses.map((course) => (
-            <TableRow key={course.name}>
-              {headerCells.map((headerCell, index) => (
-                <TableCell
-                  key={headerCell.accessor}
-                  align={index === 0 ? "left" : "right"}
-                >
-                  {course[headerCell.accessor]}
-                </TableCell>
-              ))}
-            </TableRow>
-          ))}
+          {courses
+            .slice(page * rowsPerPage, (page + 1) * rowsPerPage)
+            .map((course) => (
+              <TableRow key={course.name}>
+                {headerCells.map((headerCell, index) => (
+                  <TableCell
+                    key={headerCell.accessor}
+                    align={index === 0 ? "left" : "right"}
+                  >
+                    {course[headerCell.accessor]}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={courses.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
     </TableContainer>
   );
 }
