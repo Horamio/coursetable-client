3dcf256add3c1bafc1d86b218bef2cb2d8e3b7c3
Ricardo Yrupailla <ry.yrupailla@gmail.com>
2020-10-26 09:30:53 -0500

    Add course input

--- src/components/CourseFilter.jsx
+++ src/components/CourseFilter.jsx
@@ -7,11 +7,30 @@ import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import MenuItem from "@material-ui/core/MenuItem";
+import AddIcon from "@material-ui/icons/Add";
+import Button from "@material-ui/core/Button";
 
 // Helpers
-import { getColleges, getFaculties, getSpecialities } from "../utils";
+import {
+  getColleges,
+  getCourses,
+  getFaculties,
+  getSpecialities,
+} from "../utils";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  inputRow: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  alignRight: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingBottom: "8px",
+  },
+}));
 
 const semesters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -21,6 +40,7 @@ export default function CourseFilter({ onParamsChange = () => {} }) {
   const [faculty, setFaculty] = useState(null);
   const [speciality, setSpeciality] = useState(null);
   const [semester, setSemester] = useState("");
+  const [course, setCourse] = useState(null);
 
   const collegesQuery = useQuery("colleges", () => getColleges());
   const facultiesQuery = useQuery("faculties", () =>
@@ -29,51 +49,75 @@ export default function CourseFilter({ onParamsChange = () => {} }) {
   const specialitiesQuery = useQuery("specialities", () =>
     getSpecialities(college && college.id, faculty && faculty.id)
   );
+  const coursesQuery = useQuery("courses", () =>
+    getCourses(
+      college && college.id,
+      faculty && faculty.id,
+      speciality && speciality.id,
+      semester
+    )
+  );
 
   const onCollegeChange = (event, newValue) => {
     setCollege(newValue);
     setFaculty(null);
     setSpeciality(null);
     setSemester("");
+    setCourse(null);
   };
 
   const onFacultyChange = (event, newValue) => {
     setFaculty(newValue);
     setSpeciality(null);
     setSemester("");
+    setCourse(null);
   };
 
   const onSpecialityChange = (event, newValue) => {
     setSpeciality(newValue);
     setSemester("");
+    setCourse(null);
   };
 
   const onSemesterChange = (event) => {
     setSemester(event.target.value);
+    setCourse(null);
+  };
+
+  const onCourseChange = (event, newValue) => {
+    setCourse(newValue);
   };
 
   useEffect(() => {
-    onParamsChange(college, faculty, speciality, semester);
+    onParamsChange(college, faculty, speciality, semester, course);
   }, [
     JSON.stringify(college),
     JSON.stringify(faculty),
     JSON.stringify(speciality),
     JSON.stringify(semester),
+    JSON.stringify(course),
   ]);
 
   useEffect(() => {
     facultiesQuery.refetch();
     specialitiesQuery.refetch();
+    coursesQuery.refetch();
   }, [JSON.stringify(college)]);
 
   useEffect(() => {
     specialitiesQuery.refetch();
+    coursesQuery.refetch();
   }, [JSON.stringify(faculty)]);
+
+  useEffect(() => {
+    coursesQuery.refetch();
+  }, [JSON.stringify(speciality), semester]);
 
   if (
     collegesQuery.isLoading ||
     facultiesQuery.isLoading ||
-    specialitiesQuery.isLoading
+    specialitiesQuery.isLoading ||
+    coursesQuery.isLoading
   )
     return null;
 
@@ -131,13 +175,16 @@ export default function CourseFilter({ onParamsChange = () => {} }) {
               />
             )}
           />
+        </div>
+        <div className={classes.inputRow}>
           <TextField
-            id="combo-box-speciality"
+            id="combo-box-semester"
             select
             label="Ciclo"
             value={semester}
             onChange={onSemesterChange}
             variant="outlined"
+            style={{ width: "10ch" }}
             size="small"
             margin="normal"
           >
@@ -148,6 +195,28 @@ export default function CourseFilter({ onParamsChange = () => {} }) {
               </MenuItem>
             ))}
           </TextField>
+          <Autocomplete
+            id="combo-box-course"
+            value={course}
+            onChange={onCourseChange}
+            options={coursesQuery.data}
+            getOptionLabel={(option) => option.name}
+            style={{ width: "25ch" }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Course"
+                variant="outlined"
+                size="small"
+                margin="normal"
+              />
+            )}
+          />
+        </div>
+        <div className={classes.alignRight}>
+          <Button variant="contained" color="primary" endIcon={<AddIcon />}>
+            Agregar
+          </Button>
         </div>
       </form>
     </Paper>
