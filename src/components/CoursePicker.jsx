81f268d1c84377e87a6359f18ff3dee619915c9b
Ricardo Yrupailla <ry.yrupailla@gmail.com>
2020-10-26 04:10:46 -0500

    Separate settings popover

--- src/components/CoursePicker.jsx
+++ src/components/CoursePicker.jsx
@@ -1,20 +1,14 @@
 import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import { useQuery } from "react-query";
-import { makeStyles } from "@material-ui/core/styles";
 
 // Components
 import CourseTable from "./CourseTable";
 import CourseFilter from "./CourseFilter";
-import IconButton from "@material-ui/core/IconButton";
-import TuneIcon from "@material-ui/icons/Tune";
-import Popover from "@material-ui/core/Popover";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormGroup from "@material-ui/core/FormGroup";
+import SettingsPopover from "./SettingsPopover";
 
 // Helpers
-import { getCourses, getSections } from "../utils";
+import { getCourses } from "../utils";
 
 const StyledCoursePicker = styled.div`
   width: 400px;
@@ -69,77 +63,5 @@ export default function CoursePicker() {
         />
       </div>
     </StyledCoursePicker>
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& .MuiFormGroup-root": {
-      paddingLeft: "10px",
-    },
-  },
-}));
-
-function SettingsPopover({ course }) {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = useState(null);
-  const {
-    isLoading: isSectionsLoading,
-    data: sections,
-    refetch,
-  } = useQuery("sections" + course.id, () => getSections(course.id));
-
-  const handleClick = (event) => {
-    refetch();
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
-  const id = open ? "simple-popover" : undefined;
-
-  if (isSectionsLoading) return null;
-
-  return (
-    <div>
-      <IconButton onClick={handleClick} edge="end" aria-label="comments">
-        <TuneIcon />
-      </IconButton>
-      <Popover
-        id={id}
-        className={classes.root}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-      >
-        <FormGroup row>
-          {sections.map((section) => (
-            <FormControlLabel
-              key={section.id}
-              control={
-                <Checkbox
-                  checked={false}
-                  onChange={() => {}}
-                  inputProps={{ "aria-label": "primary checkbox" }}
-                  color="primary"
-                />
-              }
-              label={section.code}
-            />
-          ))}
-        </FormGroup>
-      </Popover>
-    </div>
   );
 }
---
+++ src/components/SettingsPopover.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import { makeStyles } from "@material-ui/core/styles";
+
+// Components
+import IconButton from "@material-ui/core/IconButton";
+import TuneIcon from "@material-ui/icons/Tune";
+import Popover from "@material-ui/core/Popover";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+
+// Helpers
+import { getSections } from "../utils";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiFormGroup-root": {
+      paddingLeft: "10px",
+    },
+  },
+}));
+
+export default function SettingsPopover({ course }) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const {
+    isLoading: isSectionsLoading,
+    data: sections,
+    refetch,
+  } = useQuery("sections" + course.id, () => getSections(course.id));
+
+  const handleClick = (event) => {
+    refetch();
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  if (isSectionsLoading) return null;
+
+  return (
+    <div>
+      <IconButton onClick={handleClick} edge="end" aria-label="comments">
+        <TuneIcon />
+      </IconButton>
+      <Popover
+        id={id}
+        className={classes.root}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <FormGroup row>
+          {sections.map((section) => (
+            <FormControlLabel
+              key={section.id}
+              control={
+                <Checkbox
+                  checked={false}
+                  onChange={() => {}}
+                  inputProps={{ "aria-label": "primary checkbox" }}
+                  color="primary"
+                />
+              }
+              label={section.code}
+            />
+          ))}
+        </FormGroup>
+      </Popover>
+    </div>
+  );
+}
