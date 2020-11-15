99b2c23fa4bdae318e00b02566f92bbbca022011
Ricardo Yrupailla <ry.yrupailla@gmail.com>
2020-11-14 21:44:03 -0500

    Add pluralize dependency

--- src/components/CoursePicker.jsx
+++ src/components/CoursePicker.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled from "@emotion/styled";
 import update from "immutability-helper";
+import pluralize from "pluralize";
 
 // Components
 import CourseTable from "./CourseTable";
